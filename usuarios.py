b82895c8aa28ceb6afe26b811d5f99985206b098
SeeYouOnTheMoon . <migue.narajo21@gmail.com>
2026-03-09 10:15:04 -0500

    se añade el while del menu

--- usuarios.py
+++ usuarios.py
@@ -1,6 +1,6 @@
-from colorama import Fore,Style, init
+from colorama import Fore, init
 
-init()#
+init()
 
 def registrar_prestamo(inventario):
     print( "=" * 30)
@@ -16,9 +16,50 @@ def registrar_prestamo(inventario):
     else:
         print(Fore.RED + "[ERROR] El ID del objeto y el responsable son obligatorios." + Fore.RESET)
 
+def mostrar_reporte(inventario):
+    print("=" * 30)
+    print("   REPORTE DE INVENTARIO")
+    print("=" * 30)
+    
+    if not inventario:
+        print("No hay objetos registrados.")
+    else:
+        for obj, resp in inventario.items():
+            print(f"Objeto: {obj} - Responsable: {resp}")
+    
+    print("=" * 30)
+
+def menu_principal():
+    """
+    Función de la Persona D: Menú interactivo con ciclo while.
+    Une el trabajo de las Personas B y C.
+    """
+    inventario_escolar = {}  # Diccionario donde se guardará todo
+    
+  
+    while True:
+        print("\n" + "=".center(40, "="))
+        print(Fore.BLUE + " SISTEMA DE CONTROL DE PRESTAMOS " )
+        print("=".center(40, "="))
+        print("1. Registrar nuevo préstamo ")
+        print("2. Ver reporte de inventario ")
+        print("3. Salir")
+        print("=".center(40, "=") + Fore.RESET)
+        
+        opcion = input("Elija una opción (1-3): ")
+        
+        if opcion == "1":
+          
+            registrar_prestamo(inventario_escolar)
+        elif opcion == "2":
+         
+            mostrar_reporte(inventario_escolar)
+        elif opcion == "3":
+            print("\n abriendose del parcheÑ")
+            break  
+        else:
+            print("\n[!] Error: Opción no válida. Intente de nuevo.")
+
+
 if __name__ == "__main__":
-    trazabilidad_escolar = {}
-
-    registrar_prestamo(trazabilidad_escolar)
-
-    print(Fore.CYAN + Style.BRIGHT + "Diccionario actual:", trazabilidad_escolar)
+    menu_principal()
